a266a7340f24099a8a9a98ddaeab273bbd107855
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-12-03 19:22:12 +0100

    [WebApp] Fixed the database model, and added some examples of the OpenTrep database use.

--- gui/django/opentrep/ref_place/models.py
+++ gui/django/opentrep/ref_place/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Details (models.Model):
-	code = models.CharField (max_length=3)
+	code = models.CharField (max_length=3, primary_key=True)
 	longitude = models.FloatField()
 	latitude = models.FloatField()
+	def __unicode__(self):
+		return '%s (%d, %d)' % (self.code, self.latitude, self.longitude)
 
 class Names (models.Model):
-	place = models.ForeignKey (Details)
-	language_code = models.CharField (max_length=2)
+	code = models.ForeignKey (Details, db_column='code', primary_key=True)
+	language_code = models.CharField (max_length=2, primary_key=True)
 	classical_name = models.CharField (max_length=30)
+	def __unicode__(self):
+		return '%s (%s, %s)' % (self.code, self.language_code, self.classical_name)
 
---
+++ gui/django/opentrep/test_db.py
@@ -0,0 +1,6 @@
+from ref_place.models import Details, Names
+
+# No places are in the system yet
+Details.objects.all()
+#Names.objects.all()
+
