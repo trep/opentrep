00b019d8550737dff5d1021897c46d075b978e47
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-12-03 19:34:22 +0100

    [WebApp] Fixed the database model, and added some examples of the OpenTrep database use.

--- gui/django/opentrep/test_db.py
+++ gui/django/opentrep/test_db.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
+
+#
 from ref_place.models import Details, Names
 
-# No places are in the system yet
-Details.objects.all()
-#Names.objects.all()
+#
+details = Details.objects.all()
+names = Names.objects.all()
+
+#
+print 'Details: ', details
+
 
