6e4f9cc043064a965813df57a3dac7580d8c3add
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-12-31 00:57:57 +0100

    [Build] Improved the support of CentOS 5.x in the FindBoostWrapper.cmake and FindPythonLibsWrapper.cmake helpers.

--- config/FindPythonLibsWrapper.cmake
+++ config/FindPythonLibsWrapper.cmake
@@ -1,15 +1,17 @@
-# That file is just wrapper around the standard Fedora/RedHat FindPythonLibs.cmake
-# script. Indeed, that latter does not work well on EPEL 5, where only the Python
-# static library is searched. In that latter case, the Python dynamic library is
-# searched, and the PYTHON_LIBRARIES variable re-set accordingly.
+# That file is just a wrapper around the standard Fedora/RedHat
+# FindPythonLibs.cmake script. Indeed, that latter does not work well on
+# EPEL 5, when Python-2.6 is not installed, because only the Python (2.4) 
+# static library is found. In that latter case, the following CMake code
+# searches for the Python dynamic library, and the PYTHON_LIBRARIES variable
+# is re-set accordingly.
+#
 # Moreover, on EPEL 5 and 6, only PYTHON_INCLUDE_PATH is defined, where as
 # PYTHON_INCLUDE_DIRS is expected to be set.
-
 #
 if (PYTHON_INCLUDE_PATH AND "${PYTHON_INCLUDE_DIRS}" STREQUAL "")
   set (PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_PATH} PARENT_SCOPE)
   set (PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_PATH})
-endif (PYTHON_INCLUDE_PATH AND "${PYTHON_INCLUDE_DIRS}" STREQUAL "")
+endif ()
 
 #
 if ("${PYTHON_LIBRARIES}" MATCHES "libpython[1-9.]*.a")
@@ -19,17 +21,13 @@ if ("${PYTHON_LIBRARIES}" MATCHES "libpython[1-9.]*.a")
 
   # Set up the versions we know about, in the order we will search. Always add
   # the user supplied additional versions to the front.
-  set(_Python1_VERSIONS 1.6 1.5)
-  set(_Python2_VERSIONS 2.7 2.6 2.5 2.4 2.3 2.2 2.1 2.0)
-  set(_Python3_VERSIONS 3.3 3.2 3.1 3.0)
   set(_Python_VERSIONS ${Python_ADDITIONAL_VERSIONS}
-	${_Python1_VERSIONS} ${_Python2_VERSIONS} ${_Python3_VERSIONS})
+	2.7 2.6 2.5 2.4 2.3 2.2 2.1 2.0 1.6 1.5)
 
   foreach (_CURRENT_VERSION ${_Python_VERSIONS})
 	string (REPLACE "." "" _CURRENT_VERSION_NO_DOTS ${_CURRENT_VERSION})
 	find_library (PYTHON_DYN_LIBRARY
-      NAMES python${_CURRENT_VERSION_NO_DOTS} python${_CURRENT_VERSION}
-	  python${_CURRENT_VERSION_NO_DOTS}mu python${_CURRENT_VERSION}mu)
+      NAMES python${_CURRENT_VERSION_NO_DOTS} python${_CURRENT_VERSION})
 	if (NOT "${PYTHON_DYN_LIBRARY}" STREQUAL "")
 	  set (PYTHON_LIBRARIES ${PYTHON_DYN_LIBRARY})
 	endif (NOT "${PYTHON_DYN_LIBRARY}" STREQUAL "")
@@ -49,7 +47,7 @@ if ("${PYTHON_LIBRARIES}" MATCHES "libpython[1-9.]*.a")
   if (PYTHONLIBSWRAPPER_FOUND)
 	mark_as_advanced (PYTHONLIBSWRAPPER_FOUND)
   else (PYTHONLIBSWRAPPER_FOUND)
-	message (FATAL_ERROR "Could not find the PythonLibs libraries! Please install the development-libraries and headers (e.g., 'python-devel' for Fedora/RedHat).")
+	message (FATAL_ERROR "Could not find the PythonLibs libraries! Please install the development-libraries and headers (e.g., 'pythonlibs-devel' for Fedora/RedHat).")
   endif (PYTHONLIBSWRAPPER_FOUND)
 
 endif ("${PYTHON_LIBRARIES}" MATCHES "libpython[1-9.]*.a")
