febacad9f9bceea0c0716175dcedb7387c15033c
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2013-03-24 14:19:42 +0100

    [Dev] Renamed Travel.{hc}pp into LocationExchange.{hc}pp

--- test/protobuf/travel_protobuf.cpp
+++ test/protobuf/travel_protobuf.cpp
@@ -2,19 +2,28 @@
 #include <iostream>
 // OpenTREP
 #include <opentrep/Location.hpp>
-#include <opentrep/bom/Travel.hpp>
+#include <opentrep/bom/LocationExchange.hpp>
 #include <opentrep/service/Logger.hpp>
 
 // ////////////// M A I N //////////////
 int main (int argc, char* argv[]) {
 
 
-  //
+  // Build the POR (point of reference) corresponding to Kiev Boryspil
+  const OPENTREP::LocationKey lLocationKey (OPENTREP::IATACode_T ("KBP"),
+                                            OPENTREP::ICAOCode_T ("UKBB"),
+                                            OPENTREP::GeonamesID_T (6300952));
+
+  // Build a Location structure to hold the POR details
   OPENTREP::Location lLocation;
-  OPENTREP::fillLocation (lLocation);
+  lLocation.setKey (lLocationKey);
+
+  // Serialise the Location structure with Protobuf
+  const std::string& lSerialisedLocation =
+    OPENTREP::LocationExchange::exportLocation (lLocation);
 
   // DEBUG
-  OPENTREP_LOG_DEBUG ("Location: " << lLocation);
+  OPENTREP_LOG_DEBUG ("Serialised location: " << lSerialisedLocation);
   
   return 0;
 }
