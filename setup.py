ee805a4fba9ebfd4e84627797767e9a81568cd3a
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2020-05-11 23:03:27 +0200

    [Python] The setup.py specification takes platform into account

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-import os, setuptools
+import os, sys, setuptools
 from skbuild import setup
 
 with open("README.md", "r") as fh:
@@ -23,13 +23,14 @@ setup (
         "Operating System :: OS Independent",
     ],
     cmake_args = [
-      '-DREADLINE_ROOT=/usr/local/opt/portable-readline',
-      '-DREADLINE_INCLUDE_DIR=/usr/local/opt/portable-readline/include',
-      '-DREADLINE_LIBRARY=/usr/local/opt/libedit/lib/libedit.dylib',
-      '-DICU_ROOT=/usr/local/opt/icu4c',
       '-DINSTALL_DOC:BOOL=OFF',
       '-DRUN_GCOV:BOOL=OFF',
       '-DLIB_SUFFIX='
-      ]
+      ] + (
+      ['-DREADLINE_ROOT=/usr/local/opt/portable-readline',
+       '-DREADLINE_INCLUDE_DIR=/usr/local/opt/portable-readline/include',
+       '-DREADLINE_LIBRARY=/usr/local/opt/libedit/lib/libedit.dylib',
+       '-DICU_ROOT=/usr/local/opt/icu4c'] if sys.platform.startswith("darwin") else []
+      )
 )
 
