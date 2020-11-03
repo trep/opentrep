4bbc4eca8e212c2f958a53550770548e86e8d173
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2020-11-03 12:38:17 +0100

    [Python] Upgraded Python dependencies for the virtual env

--- ci-scripts/soci-install-debian.sh
+++ ci-scripts/soci-install-debian.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# SOCI (as of April 2019, SOCI v4.0 has still not been released)
+# SOCI (as of Novermber 2020, SOCI v4 has still not been released on Debian/Ubuntu)
 pushd /opt/soci
-patch -p1 < ./soci-debian-cmake.patch
 mkdir -p build
 pushd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DSOCI_CXX11=ON -DSOCI_TESTS=OFF ..
