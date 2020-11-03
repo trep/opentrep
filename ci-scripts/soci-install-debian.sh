c11000c2c9861989e6a287cc632d26f88b45351f
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2020-11-03 19:57:21 +0100

    [CI] Attempt to fix the CI pipeline

--- ci-scripts/soci-install-debian.sh
+++ ci-scripts/soci-install-debian.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# SOCI (as of Novermber 2020, SOCI v4 has still not been released on Debian/Ubuntu)
-pushd /opt/soci
-mkdir -p build
-pushd build
+SOCI_VER="4.0.1"
+SOCI_DIR="/opt/soci"
+
+# SOCI (as of November 2020, SOCI v4 has still not been released on Debian/Ubuntu)
+mkdir -p ${SOCI_DIR}/archives
+wget https://github.com/SOCI/soci/archive/${SOCI_VER}.tar.gz -O ${SOCI_DIR}/archives/soci-${SOCI_VER}.tar.gz
+pushd ${SOCI_DIR}
+tar zxf archives/soci-${SOCI_VER}.tar.gz
+mkdir -p soci-${SOCI_VER}/build
+pushd soci-${SOCI_VER}/build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DSOCI_CXX11=ON -DSOCI_TESTS=OFF ..
 sudo make install
 popd
